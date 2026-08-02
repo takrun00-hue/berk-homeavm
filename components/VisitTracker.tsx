964fd79c9434f57f6a516cfc0fbe781372be400f
takrun00-hue <tak.run00@gmail.com>
2026-08-02 13:17:58 +0300

    Create VisitTracker.tsx

---
+++ components/VisitTracker.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+
+export default function VisitTracker() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    fetch("/api/visit", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ page: pathname }),
+    }).catch(() => {});
+  }, [pathname]);
+
+  return null;
+}
