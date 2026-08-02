f6a7dab334af75a39fcab51733b6495acc44b8e8
takrun00-hue <tak.run00@gmail.com>
2026-08-02 15:56:20 +0300

    Replace TelegramButton with SiteChatWidget

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import MobileBottomNav from "@/components/MobileBottomNav";
 import VisitTracker from "@/components/VisitTracker";
-import TelegramButton from "@/components/TelegramButton";
+import SiteChatWidget from "@/components/SiteChatWidget";
 import { LanguageProvider } from "@/context/LanguageContext";
 import { CartProvider } from "@/context/CartContext";
 import { AuthProvider } from "@/context/AuthContext";
@@ -29,7 +29,7 @@ export default function RootLayout({
               <Header />
               <main>{children}</main>
               <Footer />
-              <TelegramButton />
+              <SiteChatWidget />
               <MobileBottomNav />
             </CartProvider>
           </AuthProvider>
