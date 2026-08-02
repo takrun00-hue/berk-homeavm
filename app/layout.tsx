638c8e67aba591c876ccf846bdebb37967cf3c2b
takrun00-hue <tak.run00@gmail.com>
2026-08-02 13:19:08 +0300

    Add VisitTracker component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import MobileBottomNav from "@/components/MobileBottomNav";
+import VisitTracker from "@/components/VisitTracker";
 import { LanguageProvider } from "@/context/LanguageContext";
 import { CartProvider } from "@/context/CartContext";
 import { AuthProvider } from "@/context/AuthContext";
@@ -23,6 +24,7 @@ export default function RootLayout({
         <LanguageProvider>
           <AuthProvider>
             <CartProvider>
+              <VisitTracker />
               <Header />
               <main>{children}</main>
               <Footer />
