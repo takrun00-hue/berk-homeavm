cd6f229ebfb18d841aed6c961a7b067366c3577d
takrun00-hue <tak.run00@gmail.com>
2026-08-06 15:23:50 +0300

    Add Vercel storage blob hostnames to config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+      },
+      {
+        protocol: "https",
+        hostname: "*.blob.vercel-storage.com",
+      },
     ],
   },
 };
